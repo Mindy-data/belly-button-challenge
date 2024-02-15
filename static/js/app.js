f2cdd996c294896f46d354f5a2bab351737d775f
Mindy-data <mindy.musch@gmail.com>
2024-02-15 14:47:41 -0600

    Final push

--- static/js/app.js
+++ static/js/app.js
@@ -20,34 +20,36 @@ function init() {
 }
 
 
-function createPanel(mindy) {
+function createPanel(var1) {
     d3.json(url).then(function (data) {
         let metadata = data.metadata;
-        let results = metadata.filter(x => x.id == mindy)[0];
+        let results = metadata.filter(x => x.id == var1)[0];
         // console.log(results);
 
         let PANEL = d3.select('#sample-metadata');
         PANEL.html('')
         for (key in results) {
-        PANEL.append('h6').text(`${key.toUpperCase()}: ${results[key]}`)            
+            PANEL.append('h6').text(`${key.toUpperCase()}: ${results[key]}`)
         }
 
     });
 }
 
-function createCharts(kelli) {
+function createCharts(var2) {
     d3.json(url).then(function (data) {
         let samples = data.samples;
-        let results = samples.filter(x => x.id == kelli)[0];
-        console.log(results);
+        let results = samples.filter(x => x.id == var2)[0];
+        // console.log(results);
         let otu_ids = results.otu_ids;
         let sample_values = results.sample_values;
-        let otu_labels = results.otu_labels; 
+        let otu_labels = results.otu_labels;
 
         let bLayout = {
-            title: 'Bubble'
-             
+            xaxis: {
+                title: 'OTU IDS'
+            }
         };
+
         let bData = [{
             x: otu_ids,
             y: sample_values,
@@ -62,26 +64,24 @@ function createCharts(kelli) {
 
         Plotly.newPlot('bubble', bData, bLayout);
 
+        let yNew = otu_ids.slice(0, 10).map(x => `OTU ${x}`).reverse()
 
+        let barLayout = {
+            title: 'Top 10 OTUs',
+            margin: {
+                l: 150,
+            }
+        };
 
+        let barData = [{
+            x: sample_values.slice(0, 10).reverse(),
+            y: yNew,
+            text: otu_labels.slice(0, 10).reverse(),
+            type: 'bar',
+            orientation: 'h'
+        }];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        Plotly.newPlot('bar', barData, barLayout);
 
 
 
